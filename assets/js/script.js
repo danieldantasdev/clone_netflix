81daa8bb1c794771e10ace9c1f5dd52524476d3b
lobinhodev <contatolobinhodev@gmail.com>
2022-02-06 21:20:13 -0300

    Adicionando a biblioteca Swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,45 @@ menuHamburguer.addEventListener('click', () => {
         .querySelector('.l-nav__container')
         .classList.toggle('show-menu')
 })
+
+/* ==================== Slider swiper ==================== */
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+    },
+    autoplay: {
+        delay: 1000
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    mousewheel: true,
+    keyboard: true,
+    grabCursor: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        767: {
+            slidesPerView: 8,
+            setWrapperSize: true
+        }
+    }
+})
